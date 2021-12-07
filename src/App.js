86bc765aebe23b64ab5276fd336a1f1f3849c3ba
Xavier <xavierfeixesguell@gmail.com>
2021-12-07 19:35:41 +0100

    added props and finished project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import Header from './components/Header'
 import Card from './components/Card'
+import data from './data'
+
+
 
 const App = () => {
+
+    const cards = data.map(card => {
+        return (<Card
+            key={card.id}
+            {...card}
+        />)
+    })
+
     return (
         <div>
             <Header />
-            <Card />
+            {cards}
         </div>
 
     )
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,27 +1,24 @@
 import React from 'react'
 
-const Card = () => {
+const Card = (props) => {
+    console.log(props)
     return (
 
         <div className='card'>
             <img
                 className='card-image'
-                src="../images/mount-fuji.jpg"
-                alt="Mount Fuji"
+                src={props.imageUrl}
+                alt={props.title}
             />
             <div className='card-info'>
                 <p className='card-location'>
-                    <span className='card-icon'><i class="fa-solid fa-location-dot"></i></span>
-                    <span className='card-country'>Japan</span>
-                    <a className='card-link' href="https://goo.gl/maps/1DGM5WrWnATgkSNB8" target='_blank'>View on Google Maps</a>
+                    <span className='card-icon'><i className="fa-solid fa-location-dot"></i></span>
+                    <span className='card-country'>{props.location}</span>
+                    <a className='card-link' href={props.googleMapsUrl} target='_blank'>View on Google Maps</a>
                 </p>
-                <h2 className='card-title'>Mount Fuji</h2>
-                <p className='card-dates'>12 Jan, 2021 - 12 Jan, 2021</p>
-                <p className='card-text'>
-                    Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters
-                    (12,380 feet). Mount Fuji is the single most popular tourist site in Japan,
-                    for both Japanese and foreign tourists.",
-                </p>
+                <h2 className='card-title'>{props.title}</h2>
+                <p className='card-dates'>{props.startDate} - {props.endDate}</p>
+                <p className='card-text'>{props.description}</p>
             </div>
         </div>
     )
