939353faa412fe64f22592ef36406c02fe57109b
Xavier <xavierfeixesguell@gmail.com>
2021-12-07 17:58:14 +0100

    header section

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Header = () => {
+    return (
+        <header>
+            <span className='world-icon'><i className="fa-solid fa-earth-africa"></i></span>
+            <p>my travel journal.</p>
+        </header>
+    )
+}
+
+export default Header
